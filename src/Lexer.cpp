82b5c988c23c08879106b68cbb0d8e1aa0ca9939
Tiago Silveira <tiagojesusilveira@gmail.com>
2026-01-28 09:30:52 +0100

    Debug: Lexer was not displaying all tokens

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -17,8 +17,14 @@ bool	is_delim(std::string &config_str, int loc, std::vector<char> &delim)
 {
 	for (size_t i = 0; i < delim.size(); i++)
 	{
+		// std::cout << "*This is a delim* [" << delim[i] <<"]\n";
+
 		if (config_str[loc] == delim[i])
+		{
+			// std::cout << "*This is a delim* [" << config_str[loc] <<"]\n";
 			return true;
+
+		}
 	}
 	return false;
 }
@@ -33,7 +39,12 @@ bool	is_end_of_token(std::string &config_str, int curr_i, std::vector<char>& del
 Lexer::Lexer(std::string &config_str)
 {
 	char delim_array[] = {'{', '}', '\"', ';', '#'};
-	std::vector<char> delim(delim_array, delim_array + sizeof(delim_array) / sizeof(delim_array));
+	std::vector<char> delim(delim_array, delim_array + sizeof(delim_array) / sizeof(char));
+	for (size_t i = 0; i < delim.size(); i++)
+	{
+		std::cout << "This is delim ->" << delim[i] <<"\n";
+	}
+	
 	std::string temp_str;
 
 	for (int i = 0; config_str[i]; i++)
@@ -74,10 +85,9 @@ Lexer::Lexer(std::string &config_str)
 			while (is_end_of_token(config_str, i, delim) == false)
 			{
 				temp_str += config_str[i];
-				std::cout << "[This is config_str[i]]: " << config_str[i] << std::endl;
 				i++;
 			}
-			std::cout << "This is temp_str: " << temp_str << std::endl;
+			i--;
 			if (std::find(Server::directives.begin(), Server::directives.end(), temp_str) != Server::directives.end())
 				_token_list.push_back(token(temp_str, KeywordTok));
 			else
@@ -91,7 +101,9 @@ std::ostream& operator<<(std::ostream& os, const Lexer &obj)
 	os <<"Beginning of the Lexer\n";
 	for (std::list<Token>::const_iterator it = obj._token_list.begin(); it != obj._token_list.end(); it++)
 	{
+		// Debug
 		os << "Content: " <<it->content << "; type: " << it->type << std::endl; 
+		// os << it->content ; 
 	}
 	return(os);
 }
